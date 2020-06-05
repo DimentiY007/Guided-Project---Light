340f1d2ef781f27ac1927386a99902264d1036a3
Dmitry Stekvashov <dimastekv@gmail.com>
2020-06-05 08:37:33 +0300

    Initial Commit

---
+++ Guided Project - Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guided Project - Light
+//
+//  Created by Dmitriy Stekvashov on 05.06.2020.
+//  Copyright © 2020 Dmitriy Stekvashov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
