d529893537073cbb546fe44081197c9a2d1df93c
Dmitry Stekvashov <dimastekv@gmail.com>
2020-06-05 10:16:52 +0300

    well done

--- Guided Project - Light/ViewController.swift
+++ Guided Project - Light/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var isLightOn = true
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        isLightOn = !isLightOn
+        updateView()
+        
+    }
+    func updateView(){
+        let device = AVCaptureDevice.default(for: AVMediaType.video)
+        if let dev = device, dev.hasTorch{
+            view.backgroundColor = .black
+            do{
+                try dev.lockForConfiguration()
+                dev.torchMode = isLightOn ? .on : .off
+                dev.unlockForConfiguration()
+            }catch{
+                print(error)
+            }
+            
+            }else{
+            view.backgroundColor = isLightOn ? .white : .black
+        }
 
+    }
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateView()
     }
 
 
